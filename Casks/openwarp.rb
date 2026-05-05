8940ceb4e7b4041ff6b511ee281cabbc732505ee
Leo <450019458@qq.com>
2026-05-05 22:57:53 +0800

    chore(release): 发布 OpenWarp 2026.05.05.1

--- Casks/openwarp.rb
+++ Casks/openwarp.rb
@@ -1,6 +1,6 @@
 cask "openwarp" do
   version "2026.05.05.1"
-  sha256 :no_check
+  sha256 "c4abc83cc7e50d6dca3341b954f4a26890600fc7f0500fca5886932af7cf3d1b"
 
   url "https://github.com/LeoYoung-code/warp/releases/download/openwarp-v#{version}/OpenWarp-arm64.dmg",
       verified: "github.com/LeoYoung-code/warp/"
