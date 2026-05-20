94125e1f33ba9f284d1c754be355abbc53c94ab1
Leo <450019458@qq.com>
2026-05-20 16:04:15 +0800

    fix(release): 修正 OpenWarp 2026.05.20.1 校验和

--- Casks/openwarp.rb
+++ Casks/openwarp.rb
@@ -1,6 +1,6 @@
 cask "openwarp" do
   version "2026.05.20.1"
-  sha256 "127265d0872785c2f74e68ed23d9aae070f40df227600123272fcb6b597bc832"
+  sha256 "2ecc8f19cd87914bd7212e576a7556d7b8c254f55bb8b75e1aecba328d253ead"
 
   url "https://github.com/LeoYoung-code/warp/releases/download/v#{version}/OpenWarp-arm64.dmg"
   name "OpenWarp"
